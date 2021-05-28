1547425993a91a7064fe90bada92004cf6b4ba15
Matt Williams <m@technovangelist.com>
2021-05-28 14:49:43 -0700

    asht
    Signed-off-by: Matt Williams <m@technovangelist.com>

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 git clone https://github.com/technovangelist/aws-datadog-workshop-sourcefiles.git ~/sourcefiles 
 
 cp -r ~/sourcefiles/section* ~/environment/
+chmod +x ~/environment/section2/setup.sh
 npm i -g c9
 sudo yum install terraform -y
 
@@ -25,6 +26,6 @@ ssh-keygen -f ~/.ssh/workshop -N ""
 chmod 0600 ~/.ssh/workshop
 cp ~/.ssh/workshop ~/environment/section1/ecommerceapp
 
-aws ec2 import-key-pair --key-name ecommerceapp --public-key-material fileb://~/environment/section1/ecommerceapp.pub
+aws ec2 import-key-pair --key-name ecommerceapp --public-key-material fileb://~/.ssh/workshop.pub
 aws ec2 run-instances --image-id ami-07e965b5b43bda762 --count 1 --instance-type t3.medium --key-name ecommerceapp --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=workshopcluster}]'
 
