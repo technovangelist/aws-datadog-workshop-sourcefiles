f21d7041445d33527345c669c3ce006050d0402b
Matt Williams <m@technovangelist.com>
2021-05-30 14:38:09 -0700

    asht
    Signed-off-by: Matt Williams <m@technovangelist.com>

--- install.sh
+++ install.sh
@@ -22,11 +22,49 @@ curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s http
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
+curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+helm repo add datadog https://helm.datadoghq.com
+helm repo add stable https://charts.helm.sh/stable
+helm repo update
 
 ssh-keygen -f ~/.ssh/workshop -N ""
 chmod 0600 ~/.ssh/workshop
 cp ~/.ssh/workshop ~/environment/section1/ecommerceapp
+cp ~/.ssh/workshop.pub ~/environment/section1/ecommerceapp.pub
 
 aws ec2 import-key-pair --key-name ecommerceapp --public-key-material fileb://~/.ssh/workshop.pub
-aws ec2 run-instances --image-id ami-07e965b5b43bda762 --count 1 --instance-type t3.medium --key-name ecommerceapp --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=workshopcluster}]'
+
+aws iam create-group --group-name kops
+
+aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonEC2FullAccess --group-name kops
+aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonRoute53FullAccess --group-name kops
+aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess --group-name kops
+aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/IAMFullAccess --group-name kops
+aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonVPCFullAccess --group-name kops
+
+aws iam create-user --user-name kops
+aws iam add-user-to-group --user-name kops --group-name kops
+aws iam create-access-key --user-name kops > kopskeys
+
+aws configure set aws_access_key_id $(jq -r .AccessKey.AccessKeyId kopskeys)
+aws configure set aws_secret_access_key $(jq -r .AccessKey.SecretAccessKey kopskeys)
+aws configure set default.region us-west-2
+aws configure set default.output json
+
+export AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)
+export AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
+
+aws s3api create-bucket --bucket workshop-state-store --region us-east-1
+aws s3api put-bucket-versioning --bucket workshop-state-store  --versioning-configuration Status=Enabled
+aws s3api put-bucket-encryption --bucket workshop-state-store --server-side-encryption-configuration '{"Rules":[{"ApplyServerSideEncryptionByDefault":{"SSEAlgorithm":"AES256"}}]}'
+
+export KOPSNAME=workshop.k8s.local
+export KOPS_STATE_STORE=s3://workshop-state-store
+
+ssh-keygen -f /home/ec2-user/.ssh/id_rsa -q -N ""
+kops create cluster -f ./kopsconfig.yaml
+kops update cluster ${KOPSNAME} --yes --admin
+
+kops export kubecfg --admin
+# aws ec2 run-instances --image-id ami-07e965b5b43bda762 --count 1 --instance-type t3.medium --key-name ecommerceapp --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=workshopcluster}]'
 
--- section2/setup.sh
+++ section2/setup.sh
@@ -1,2 +1,3 @@
 sftp -i ~/.ssh/ecommerceapp bitnami@$(aws ec2 describe-instances --filters "Name=tag:Name,Values=workshopcluster" | jq -r .Reservations[0].Instances[0].PublicIpAddress):/etc/kubernetes/admin.conf
 mv admin.conf ~/.kube/sandbox.conf
+chmod 600 ~/.kube/sandbox.conf
